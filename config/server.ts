c8d860f0f101c2f3b97fe9b33a965d0267fba139
Misael Acosta <acostamisael166@gmail.com>
2026-03-16 15:35:18 -0300

    cambios

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,6 @@
-/*Render*/
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL'),
   app: {
     keys: env.array('APP_KEYS'),
   },
