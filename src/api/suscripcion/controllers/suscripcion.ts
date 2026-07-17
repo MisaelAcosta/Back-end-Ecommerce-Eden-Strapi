91269d76d1b62b6a71719d34a57385557bbb1c8e
Misael Acosta <acostamisael166@gmail.com>
2026-07-16 22:23:08 -0400

    categorias y suscripciones

---
+++ src/api/suscripcion/controllers/suscripcion.ts
@@ -0,0 +1,7 @@
+/**
+ * suscripcion controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::suscripcion.suscripcion');
---
+++ src/api/suscripcion/routes/suscripcion.ts
@@ -0,0 +1,7 @@
+/**
+ * suscripcion router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::suscripcion.suscripcion');
